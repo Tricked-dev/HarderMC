3b3bf6315a555a1b6740f6aee149800bd5f04f73
Tricked <tricked@tricked.pro>
2023-07-13 18:12:17 +0200

    cool server icon

--- src/main/kotlin/dev/tricked/hardermc/server/features/ServerMotD.kt
+++ src/main/kotlin/dev/tricked/hardermc/server/features/ServerMotD.kt
@@ -12,10 +12,16 @@ import dev.tricked.hardermc.utilities.Name
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.server.ServerListPingEvent
+import org.bukkit.util.CachedServerIcon
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
 import java.util.*
+import javax.imageio.ImageIO
 
 @Name("Server MotD")
 @Description("Server MOTD feature")
@@ -31,6 +37,17 @@ class ServerMotD(mc: HarderMC) : BaseTool(mc), Listener {
 
         val colorChoices = listOf(NamedTextColor.RED, NamedTextColor.GREEN, NamedTextColor.BLUE, NamedTextColor.AQUA)
         val randomColor = colorChoices[random.nextInt(colorChoices.size)]
+
+        val iconId = random.nextInt(36) + 1
+
+        val icon = {}.javaClass.getResourceAsStream("/icons/$iconId.png")
+
+
+        val image = ImageIO.read(icon)
+        val serverIcon = Bukkit.loadServerIcon(image)
+        event.setServerIcon(
+            serverIcon
+        )
 
         val motd = Component.text("")
             .append(
@@ -51,5 +68,20 @@ class ServerMotD(mc: HarderMC) : BaseTool(mc), Listener {
             motd
         )
         event.maxPlayers = maxPlayers
+    }
+
+    @Throws(IOException::class)
+    fun inputStreamToBase64(inputStream: InputStream): String? {
+        // Step 1: Read input stream and store in byte array
+        val outputStream = ByteArrayOutputStream()
+        val buffer = ByteArray(4096)
+        var bytesRead: Int
+        while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+            outputStream.write(buffer, 0, bytesRead)
+        }
+        val data = outputStream.toByteArray()
+
+        // Step 2: Encode byte array to Base64
+        return Base64.getEncoder().encodeToString(data)
     }
 }
